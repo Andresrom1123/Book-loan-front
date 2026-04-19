1c571783daea3d718dc60b959598ce3374fe9b68
Andres Romero <andres2016_romero@hotmail.com>
2026-04-19 16:44:05 -0600

    feat: add react-router-dom and set up app routing with inventory route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,9 @@
-import { LoginPage } from '../features/auth/ui/LoginPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import CategoryPage from '../features/inventory/ui/CategoryPage';
+
 import { Header } from '../shared/ui/Header/Header';
+import { LoginPage } from '../features/auth/ui/LoginPage';
 import { useTheme } from '../shared/hooks/useTheme';
 
 import styles from './App.module.css';
@@ -8,9 +12,14 @@ export function App() {
   const { dark, toggle } = useTheme();
 
   return (
-    <div className={styles.layout}>
-      <Header dark={dark} onToggle={toggle} />
-      <LoginPage />
-    </div>
+    <BrowserRouter>
+      <div className={styles.layout}>
+        <Header dark={dark} onToggle={toggle} />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/inventory" element={<CategoryPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
